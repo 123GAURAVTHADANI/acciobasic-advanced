5e0e521f8bbdd3c238cdb495a74804acb3872793
123GAURAVTHADANI <garv.thadani@gmail.com>
2024-12-10 22:17:51 +0530

    Changes

---
+++ index.js
@@ -0,0 +1,60 @@
+// console.log("Hello!");
+// data types ?
+
+// obj only in object and defining properties of an object
+// var a = BigInt(1234452323232338888328382382382832838238238);
+// print on the console
+// console.log(typeof a);
+
+// int a = "10"
+
+// var a = 10;
+// console.log(typeof a);
+// Math operation
+
+// var a ="1sasasasasasasas"
+// console.log(typeof a)
+
+// var flightData = {
+//   flightNo: "6E345",
+//   arrivalTime: "16:05",
+//   departureTime: "14:30",
+//   staffDetails: {
+//     captainName: {},
+//   },
+// };
+
+// delete flightData.arrivalTime;
+
+// // flightData.staffDetails.captainName.firstName = "Sudhanshu";
+// flightData.flyTime = {
+//   inMins: "120mins",
+//   inHrs: "2hrs",
+// };
+// console.log(flightData);
+
+// var flightData2 = {
+//   flightNo: "6E345",
+//   arrivalTime: "16:05",
+//   departureTime: "14:30",
+//   staffDetails: {
+//     captainName: {
+//       firstName: "Gaurav",
+//     },
+//   },
+// };
+
+// // var abc1 = "abc";
+// // var abc2 = "abc";
+
+// // console.log(abc1 == abc2);
+// // console.log(JSON.stringify(flightData) == JSON.stringify(flightData2));
+
+// // flightData.flightNo = "Air India";
+// // console.log(flightData.flightNo);
+// // dot notation to access
+
+// // console.log(flightData["staffDetails"].captainName.firstName);
+
+var input_array = [1, 2, 3, 4];
+console.log(typeof input_array);
